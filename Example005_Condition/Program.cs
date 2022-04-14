43dda2ea65068cbfde25ac1b8285f1fd0108d7b5
Abylai <aba.omar@mail.ru>
2022-04-14 14:23:38 +0600

    add Example 005 Condition

---
+++ Example005_Condition/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "алуа")
+{
+        Console.WriteLine("Ура, это же АЛУА!");
+        
+}
+else
+{
+    Console.Write("Привет ");
+    Console.WriteLine(username);
+}
